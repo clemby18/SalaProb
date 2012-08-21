c635540179539a648d27c214287e6b0734b577fb
clemby18 <clemby18@wp.pl>
2012-08-21 15:53:32 +0200

    u before verbose_name and add ip in class Comment

--- aplikacja/models.py
+++ aplikacja/models.py
@@ -3,12 +3,12 @@
 from django.db import models
 
 class Room(models.Model):
-    name = models.CharField(u'Nazwa Sali', max_length=20)
-    address = models.CharField(u'Adres Sali',max_length=50)
-    acoustics = models.CharField(u'Akustyka Sali', max_length=20)
-    price = models.FloatField(u'Cena Za Godzine', max_length=10)
-    instruments = models.CharField(u'Dostepne Instrumenty', max_length=200)
-    equipment = models.CharField(u'Dostepny Sprzet', max_length=200)
+    name = models.CharField(u'Nazwa sali', max_length=20)
+    address = models.CharField(u'Adres sali',max_length=50)
+    acoustics = models.CharField(u'Akustyka sali', max_length=20)
+    price = models.FloatField(u'Cena za godzine', max_length=10)
+    instruments = models.CharField(u'Dostępne instrumenty', max_length=200)
+    equipment = models.CharField(u'Dostępny sprzęt', max_length=200)
 
     class Meta:
         verbose_name = "Room"
@@ -19,9 +19,10 @@ class Room(models.Model):
 
 
 class Comment(models.Model):
-    room = models.ForeignKey(Room, verbose_name='Sala')
-    text = models.TextField(verbose_name='Opinie')
-    date = models.DateTimeField(verbose_name='Data dodania')
+    room = models.ForeignKey(Room, verbose_name=u'Sala')
+    text = models.TextField(verbose_name=u'Opinie')
+    date = models.DateTimeField(verbose_name=u'Data dodania')
+    ip = models.IPAddressField(verbose_name=u'Adres ip')
 
 
     class Meta:
