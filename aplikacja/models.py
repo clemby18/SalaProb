cd9104ac40314eb98949d8e0d29d05f6c1981b4e
clemby18 <clemby18@wp.pl>
2012-08-23 23:56:49 +0200

    very small changes

--- aplikacja/models.py
+++ aplikacja/models.py
@@ -37,6 +37,7 @@ class Comment(models.Model):
 class User(models.Model):
     first_name = models.CharField(u'Imię', max_length=20)
     last_name = models.CharField(u'Nazwisko', max_length=20)
+    email = models.EmailField(u'E-mail')
     username = models.CharField(u'Login', max_length=20)
     password = models.CharField(u'Hasło', max_length=20)
 
