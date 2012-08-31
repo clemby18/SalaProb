1456067974e42d950f84f1b251f7951dc429508e
clemby18 <clemby18@wp.pl>
2012-08-31 18:06:52 +0200

    more changes in templates

--- urls.py
+++ urls.py
@@ -2,19 +2,21 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
 
-
 admin.autodiscover()
 
-urlpatterns = patterns('aplikacja.views',
+urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    (r'^$', 'index'),
-    (r'^opinions/(?P<id_category>\d+)', 'opinions'),
-    (r'^index/', 'index'),
-    (r'^add_opinion/(?P<id>\d+)', 'add_opinion'),
-    (r'^success/', 'success'),
-    (r'^registration/', 'registration'),
-    (r'^login/', 'my_login'),
-    (r'^invalid_login/', 'invalid_login'),
-    (r'^inactive_account/', 'inactive_account'),
-    (r'^logout/', 'my_logout'),
+)
+
+urlpatterns += patterns('accounts.forms',
+    url(r'^register/$', 'register'),
+    url(r'^login/$', 'login_view'),
+    url(r'^done/$', 'done'),
+)
+
+urlpatterns += patterns('rooms.views',
+    #url(r'$', 'index'),
+    url(r'^index/', 'index'),
+    url(r'^opinions/(?P<id_category>\d+)', 'opinions'),
+    url(r'^add_opinion/(?P<id>\d+)', 'add_opinion'),
 )
